32d6ec6883414612b2a91f72941394f7fe02072d
Elijah Banjo <elijahbanjo@gmail.com>
2024-04-11 12:24:25 +0100

    Day 10:FizzBuzz

--- day-10/fizzbuzz.go
+++ day-10/fizzbuzz.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func main(){
-	for n:= range 100{
+	for n:=0; n<100; n++{
 		if n%5 == 0 && n %3 == 0{
 			fmt.Println("Fizzbuzz")
-		}
-		else if n%5 == o{
+		} else if n%5 == 0{
 			fmt.Println("Fizz")
-		}
-		else if n%3 == 0{
+		} else if n%3 == 0{
 			fmt.Println("Buzz")
-		}
-		else{
+		} else{
 			fmt.Println(n)
 		}
 
